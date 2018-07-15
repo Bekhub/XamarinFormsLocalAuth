2cd36932107d8ac4cbb53139e9ce3fc4d82916ae
Bek <Bekvevo@gmail.com>
2018-07-15 13:09:00 +0200

    Add project files.

---
+++ UPMC/UPMC.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using RealSimpleCircle.Android;
+using Plugin.CurrentActivity;
+using Plugin.Fingerprint;
+
+namespace XamarinFormsLocalAuth.Droid
+{
+    [Activity(Label = "XamarinFormsLocalAuth", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            
+            base.OnCreate(bundle);
+    
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            CrossCurrentActivity.Current.Init(this, bundle);
+            CrossFingerprint.SetCurrentActivityResolver(() => CrossCurrentActivity.Current.Activity);
+            CircleRenderer.Init();
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ UPMC/UPMC/LocalLoginPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsLocalAuth
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocalLoginPage : ContentPage
+    {
+        private string pin = string.Empty;
+        public LocalLoginPage()
+        {
+            InitializeComponent();
+
+        }
+        private void pinBtn_Clicked(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (pin.Count() < 4)
+            {
+                pin += btn.Text;
+            }
+            else
+                pin = "";
+            switch (pin.Count())
+            {
+                case 0:
+                    firstCircle.Active = false;
+                    secondCircle.Active = false;
+                    thirdCircle.Active = false;
+                    lastCircle.Active = false;
+                    break;
+                case 1:
+                    firstCircle.Active = true;
+                    break;
+                case 2:
+                    secondCircle.Active = true;
+                    break;
+                case 3:
+                    thirdCircle.Active = true;
+                    break;
+                case 4:
+                    lastCircle.Active = true;
+                    break;
+                    //default:
+            }
+        }
+        private async void faceIdBtn_Clicked(object sender, EventArgs e)
+        {
+            var result = await CrossFingerprint.Current.AuthenticateAsync("Prove you have fingers!");
+            var f = await CrossFingerprint.Current.IsAvailableAsync();
+            if (result.Authenticated)
+            {
+                await Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                // not allowed to do secret stuff :(
+            }
+        }
+    }
+}
